1e243027a32444125f2458547c3a68fed4d89cb1
pavzaj <pavzaj@gmail.com>
2017-03-07 19:33:42 +0100

    Checking script bahaviour

--- POCLibrary/InitializeSonarqubeAnalysis.bat
+++ POCLibrary/InitializeSonarqubeAnalysis.bat
@@ -1,3 +1,4 @@
 echo Sonarqube analysis for: %APPVEYOR_REPO_COMMIT%
+echo prnumber %APPVEYOR_PULL_REQUEST_NUMBER%
 @echo off
 packages\MSBuild.SonarQube.Runner.Tool.1.0.0\tools\SonarQube.Scanner.MSBuild.exe begin /k:"SonarqubeGitHubPullRequestAnalysisPOC" /d:"sonar.host.url=https://sonarqube.com" /v:"%APPVEYOR_REPO_COMMIT%" /d:"sonar.cs.opencover.reportsPaths=OpencoverTestCoverageReport.xml" /d:sonar.cs.nunit.reportsPaths=nunit2TestResult.xml" /d:"sonar.login=%sonarqube_auth_token%"
