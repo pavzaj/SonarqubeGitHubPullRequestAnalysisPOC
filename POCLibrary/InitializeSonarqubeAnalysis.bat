919c683a8695cca829063798f144e5dcf62d54a1
pavzaj <pavzaj@gmail.com>
2017-03-07 19:44:15 +0100

    Experimenting with env conditions in cmd

--- POCLibrary/InitializeSonarqubeAnalysis.bat
+++ POCLibrary/InitializeSonarqubeAnalysis.bat
@@ -1,4 +1,11 @@
 echo Sonarqube analysis for: %APPVEYOR_REPO_COMMIT%
-echo prnumber %APPVEYOR_PULL_REQUEST_NUMBER%
-@echo off
-packages\MSBuild.SonarQube.Runner.Tool.1.0.0\tools\SonarQube.Scanner.MSBuild.exe begin /k:"SonarqubeGitHubPullRequestAnalysisPOC" /d:"sonar.host.url=https://sonarqube.com" /v:"%APPVEYOR_REPO_COMMIT%" /d:"sonar.cs.opencover.reportsPaths=OpencoverTestCoverageReport.xml" /d:sonar.cs.nunit.reportsPaths=nunit2TestResult.xml" /d:"sonar.login=%sonarqube_auth_token%"
+IF DEFINED	APPVEYOR_PULL_REQUEST_NUMBER
+(
+	echo prnumber %APPVEYOR_PULL_REQUEST_NUMBER%
+	echo Should be PR analysis
+)
+ELSE
+(
+	@echo off
+	packages\MSBuild.SonarQube.Runner.Tool.1.0.0\tools\SonarQube.Scanner.MSBuild.exe begin /k:"SonarqubeGitHubPullRequestAnalysisPOC" /d:"sonar.host.url=https://sonarqube.com" /v:"%APPVEYOR_REPO_COMMIT%" /d:"sonar.cs.opencover.reportsPaths=OpencoverTestCoverageReport.xml" /d:sonar.cs.nunit.reportsPaths=nunit2TestResult.xml" /d:"sonar.login=%sonarqube_auth_token%"
+)
