ee898c3053f7a0e25401ea449d10d48f9578399f
pavzaj <pavzaj@gmail.com>
2017-03-07 20:00:29 +0100

    Condition on pull request presence

--- POCLibrary/InitializeSonarqubeAnalysis.bat
+++ POCLibrary/InitializeSonarqubeAnalysis.bat
@@ -1,11 +1,10 @@
 echo Sonarqube analysis for: %APPVEYOR_REPO_COMMIT%
-IF DEFINED	APPVEYOR_PULL_REQUEST_NUMBER
-(
-	echo prnumber %APPVEYOR_PULL_REQUEST_NUMBER%
-	echo Should be PR analysis
-)
-ELSE
-(
+
+IF DEFINED APPVEYOR_PULL_REQUEST_NUMBER (
+	echo Analysing Pull Request %APPVEYOR_PULL_REQUEST_NUMBER%
+	@echo off
+	packages\MSBuild.SonarQube.Runner.Tool.1.0.0\tools\SonarQube.Scanner.MSBuild.exe begin /k:"SonarqubeGitHubPullRequestAnalysisPOC" /d:"sonar.host.url=https://sonarqube.com" /v:"%APPVEYOR_REPO_COMMIT%" /d:"sonar.cs.opencover.reportsPaths=OpencoverTestCoverageReport.xml" /d:sonar.cs.nunit.reportsPaths=nunit2TestResult.xml" /d:"sonar.login=%sonarqube_auth_token%" /d:"sonar.analysis.mode=preview" /d:"sonar.github.oauth=%github_oauth_token%" /d:"sonar.github.pullRequest=%APPVEYOR_PULL_REQUEST_NUMBER%" /d:"sonar.links.scm_dev=https://github.com/pavzaj/SonarqubeGitHubPullRequestAnalysisPOC"
+) ELSE (
 	@echo off
 	packages\MSBuild.SonarQube.Runner.Tool.1.0.0\tools\SonarQube.Scanner.MSBuild.exe begin /k:"SonarqubeGitHubPullRequestAnalysisPOC" /d:"sonar.host.url=https://sonarqube.com" /v:"%APPVEYOR_REPO_COMMIT%" /d:"sonar.cs.opencover.reportsPaths=OpencoverTestCoverageReport.xml" /d:sonar.cs.nunit.reportsPaths=nunit2TestResult.xml" /d:"sonar.login=%sonarqube_auth_token%"
 )
