3466539dde1c3263e1762d084e010f3cbf7b0b02
pavzaj <pavzaj@gmail.com>
2017-03-07 20:34:20 +0100

    Why?

--- POCLibrary/RunSonarqubeAnalysis.bat
+++ POCLibrary/RunSonarqubeAnalysis.bat
@@ -1,2 +1,2 @@
 @echo off
-packages\MSBuild.SonarQube.Runner.Tool.1.0.0\tools\SonarQube.Scanner.MSBuild.exe end /d:"sonar.login=%sonarqube_auth_token%" /d:"sonar.verbose=true"
+packages\MSBuild.SonarQube.Runner.Tool.1.0.0\tools\SonarQube.Scanner.MSBuild.exe end /d:"sonar.login=%sonarqube_auth_token%"
