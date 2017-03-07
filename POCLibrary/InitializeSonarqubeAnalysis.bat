a87a15c39eca478c7c9041d35f22cbd797cd2dcf
pavzaj <pavzaj@gmail.com>
2017-03-07 20:13:35 +0100

    Why doesnt it work?

--- POCLibrary/InitializeSonarqubeAnalysis.bat
+++ POCLibrary/InitializeSonarqubeAnalysis.bat
@@ -3,7 +3,7 @@ echo Sonarqube analysis for: %APPVEYOR_REPO_COMMIT%
 IF DEFINED APPVEYOR_PULL_REQUEST_NUMBER (
 	echo Analysing Pull Request %APPVEYOR_PULL_REQUEST_NUMBER%
 	@echo off
-	packages\MSBuild.SonarQube.Runner.Tool.1.0.0\tools\SonarQube.Scanner.MSBuild.exe begin /k:"SonarqubeGitHubPullRequestAnalysisPOC" /d:"sonar.host.url=https://sonarqube.com" /v:"%APPVEYOR_REPO_COMMIT%" /d:"sonar.cs.opencover.reportsPaths=OpencoverTestCoverageReport.xml" /d:sonar.cs.nunit.reportsPaths=nunit2TestResult.xml" /d:"sonar.login=%sonarqube_auth_token%" /d:"sonar.analysis.mode=preview" /d:"sonar.github.oauth=%github_oauth_token%" /d:"sonar.github.pullRequest=%APPVEYOR_PULL_REQUEST_NUMBER%" /d:"sonar.links.scm_dev=https://github.com/pavzaj/SonarqubeGitHubPullRequestAnalysisPOC"
+	packages\MSBuild.SonarQube.Runner.Tool.1.0.0\tools\SonarQube.Scanner.MSBuild.exe begin /k:"SonarqubeGitHubPullRequestAnalysisPOC" /d:"sonar.host.url=https://sonarqube.com" /v:"%APPVEYOR_REPO_COMMIT%" /d:"sonar.cs.opencover.reportsPaths=OpencoverTestCoverageReport.xml" /d:sonar.cs.nunit.reportsPaths=nunit2TestResult.xml" /d:"sonar.login=%sonarqube_auth_token%" /d:"sonar.analysis.mode=preview" /d:"sonar.github.oauth=%github_oauth_token%" /d:"sonar.github.pullRequest=%APPVEYOR_PULL_REQUEST_NUMBER%" /d:"sonar.links.scm_dev=https://github.com/pavzaj/SonarqubeGitHubPullRequestAnalysisPOC" /d:"sonar.verbose=true"
 ) ELSE (
 	@echo off
 	packages\MSBuild.SonarQube.Runner.Tool.1.0.0\tools\SonarQube.Scanner.MSBuild.exe begin /k:"SonarqubeGitHubPullRequestAnalysisPOC" /d:"sonar.host.url=https://sonarqube.com" /v:"%APPVEYOR_REPO_COMMIT%" /d:"sonar.cs.opencover.reportsPaths=OpencoverTestCoverageReport.xml" /d:sonar.cs.nunit.reportsPaths=nunit2TestResult.xml" /d:"sonar.login=%sonarqube_auth_token%"
